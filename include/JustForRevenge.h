c43460abf785e04458e1c4c6122acee37a0f8378
Bouuw <raphael.gavaudan-busy@epitech.eu>
2018-08-26 00:04:29 +0200

    Add background

--- include/JustForRevenge.h
+++ include/JustForRevenge.h
@@ -1,8 +1,43 @@
 #ifndef JUSTFORREVENGE_H
-#define JUSTFORREVENGE_H
+# define JUSTFORREVENGE_H
 
-#include <SFML/Window.h>
-#include <SFML/Graphics.h>
+# include		<SFML/Window.h>
+# include		<SFML/Graphics.h>
+# include		<stdlib.h>
 
+# define		BAD						42
+# define		GOOD					0
+# define		ERROR					84
+
+# define		IS_OK					int
+
+# define		WINDOW					(*game).Window.window
+# define		EVENT					(*game).Window.event
+
+typedef struct		Window
+{
+	sfRenderWindow	*window;
+	sfEvent			event;
+
+}					t_window;
+
+typedef struct		Graphic
+{
+	sfSprite		*wallpaper;
+
+}					t_graphic;
+
+typedef struct		Audio
+{
+
+}					t_audio;
+
+typedef struct		GLOBAL_GAME
+{
+	t_window		Window;
+	t_graphic		Graphic;
+	int patate;
+
+}					t_game;
 
 #endif
--- main.c
+++ main.c
@@ -1,27 +1,58 @@
 #include <JustForRevenge.h>
 
-
-
-sfRenderWindow	*create_game(void)
+IS_OK			initialize_window(sfRenderWindow **window)
 {
 	sfVideoMode video = {1980, 1080, 32};
 
-
-	sfRenderWindow_create(video, "JustForRevenge", sfDefaultStyle, NULL);
+	*window = sfRenderWindow_create(video, "JustForRevenge", sfDefaultStyle, NULL);
+	if (!*window)
+		return (BAD);
+	return (GOOD);
 }
 
-
-int		main(int argc, char **argv, char **env)
+IS_OK			define_wallpaper(sfSprite  **wallpaper)
 {
-	sfRenderWindow *window = create_game();
+	const sfTexture *texture;
+	const sfIntRect rect = {0, 0, 1980, 1080};
 
+	*wallpaper = sfSprite_create();
+	texture = sfTexture_createFromFile("./res/Wallpaper/Wallpaper.jpg", (&rect));
+	sfSprite_setTexture(*wallpaper, texture, sfFalse);
+	if (!*wallpaper)
+		return (BAD);
+	return (GOOD);
+}
 
-	while (sfRenderWindow_isOpen(window))
+t_game				*initialize_game_struct(void)
+{
+	t_game *new = malloc(sizeof(*new));
+
+	if (!new)
+		return (NULL);
+	return (new);
+}
+
+IS_OK				main(void)
+{
+	t_game *game;
+
+	if ((game = initialize_game_struct()) == NULL || initialize_window(&(WINDOW)))
+		return (ERROR);
+	if (define_wallpaper(&(*game).Graphic.wallpaper))
+		return (ERROR);
+
+	sfRenderWindow_setFramerateLimit(WINDOW, 60);
+
+	while (sfRenderWindow_isOpen(WINDOW))
 	{
-		sfRenderWindow_display(window);
-		sfRenderWindow_clear(window, sfBlue);
-
+		while (sfRenderWindow_pollEvent(WINDOW, &(EVENT)))
+		{
+			if (EVENT.type == sfEvtClosed || sfKeyboard_isKeyPressed(sfKeyEscape))
+				sfRenderWindow_close(WINDOW);
+		}
+		sfRenderWindow_display(WINDOW);
+		sfRenderWindow_clear(WINDOW, sfWhite);
+		sfRenderWindow_drawSprite(WINDOW, (*game).Graphic.wallpaper, NULL);
 	}
-
-
+	return (GOOD);
 }
